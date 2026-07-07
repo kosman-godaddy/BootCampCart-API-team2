65421619b18b19c867ae8d59cc91494d1121f228
Kamil Osman <kosman@godaddy.com>
2026-07-07 19:32:33 -0400

    all done

--- cart_api/routes/cartitems.py
+++ cart_api/routes/cartitems.py
@@ -1,7 +1,6 @@
 import falcon
 from playhouse.shortcuts import model_to_dict
-
-# from cart_api.database import DatabaseCartItem
+from cart_api.database import DatabaseCartItem
 
 
 # Exercise 3:
@@ -12,8 +11,32 @@ from playhouse.shortcuts import model_to_dict
 
 
 class CartItems:
-    pass
+    def on_get(self, req, resp):
+        items = DatabaseCartItem.select().order_by(DatabaseCartItem.id)
+        resp.media = [model_to_dict(item) for item in items]
+        resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        data = req.media
+        new_item = DatabaseCartItem.create(**data)
+        resp.media = model_to_dict(new_item)
+        resp.status = falcon.HTTP_201
 
 
 class CartItem:
-    pass
+    def on_get(self, req, resp, cart_item_id):
+        cart_item = DatabaseCartItem.get(id=cart_item_id)
+        resp.media = model_to_dict(cart_item)
+        resp.status = falcon.HTTP_200
+
+    def on_delete(self, req, resp, cart_item_id):
+        DatabaseCartItem.delete_by_id(cart_item_id)
+        resp.status = falcon.HTTP_204
+
+    def on_patch(self, req, resp, cart_item_id):
+        cart_item = DatabaseCartItem.get(id=cart_item_id)
+        data = req.media
+        for key, value in data.items():
+            setattr(cart_item, key, value)
+        cart_item.save()
+        resp.status = falcon.HTTP_204
